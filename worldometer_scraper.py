e54465b27ebc68e862630deeff8b09f2f547491f
AlanTurist <59668425+AlanTurist@users.noreply.github.com>
2020-05-22 10:03:21 +0200

    Update worldometer_scraper.py

--- worldometer_scraper.py
+++ worldometer_scraper.py
@@ -8,6 +8,6 @@ records = covid.get_data()
 
 import pandas as pd
 
-df = pd.DataFrame(records, columns = ['country', 'confirmed', 'new_cases', 'deaths', 'recovered', 'active', 'critical', 'new_deaths', 'total_tests'])
+df = pd.DataFrame(records, columns = ['country', 'confirmed', 'new_cases', 'deaths', 'recovered', 'active', 'critical', 'new_deaths', 'total_tests', 'population'])
 
 df.to_csv('today_worldwide_covid19_data.csv', index = False, encoding = 'utf-8')
