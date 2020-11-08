70bc65c2c174f276fdf75532d3cb8f68779765c1
AlanTurist <59668425+AlanTurist@users.noreply.github.com>
2020-11-08 12:07:14 +0200

    Create covid19_analysis_2.2.py

---
+++ covid19_analysis_2.2.py
@@ -0,0 +1,30 @@
+import pandas as pd
+url = 'https://raw.githubusercontent.com/AlanTurist/covid19_worldometers_scraping_and_analysis/master/today_worldwide_covid19_data.csv'
+df = pd.read_csv(url)
+
+language = str(input("\nΓια Ελληνικά πατήστε GR\n\nFor English press EN: "))
+
+if language == "GR":
+    import country_def
+    count = str(input("\nΕισάγετε χώρα ή περιοχή: "))
+    x = df[df["country"] == count]
+    country_def.country(x)
+
+elif language == "EN":
+    import country_def_en
+    count1 = str(input("\nInsert area or country: "))
+    y = df[df["country"] == count1]
+    country_def_en.country_en(y)
+else:
+    print("\nWrong language..")
+
+import os
+import sys
+
+restart = input("\n\nIf you want to try another area, press 'Y' and 'enter'.\n\nTo exit press 'enter': ")
+
+if restart == "Y":
+    os.execl(sys.executable, sys.executable, * sys.argv) 
+else:
+    print("\nExit..")
+    sys.exit(0)
